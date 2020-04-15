430a27818f4e51f01f992cbf9fe7b58824c5ddc2
noymoryosef <62888525+noymoryosef@users.noreply.github.com>
2020-04-15 22:46:04 +0300

    Add files via upload

---
+++ DocuProject/Models/ImgTeacher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DocuProject.Models
+{
+    public class ImgTeacher
+    {
+            string email;
+            string img;
+
+            public string Img { get => img; set => img = value; }
+            public string Email { get => email; set => email = value; }
+
+
+            public int insertPic2(ImgTeacher img) // הכנסת התמונה משלב2
+            {
+                DBservices dbs = new DBservices();
+                int numAffected = dbs.insertPic2(img);
+                return numAffected; //מחזיר את מספר השורות
+            }
+
+            public string getAvatarImage_(string Id)
+            {
+                DBservices dbs = new DBservices();
+                return dbs.getAvatarImage(Id,"teacher");
+            }
+
+
+
+    }
+    
+}
