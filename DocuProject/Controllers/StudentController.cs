5e644e48e933b85f619966ef6b92b566f12a30fa
ShirMal <51335669+malkashir@users.noreply.github.com>
2020-04-29 20:42:44 +0300

    שיר 29.4
    שיר להיום:
    הוספת שדה נוסף (מין) אצל התלמיד - צד שרת + לקוח -✔️
    סידור התאריכים בפורמט הנכון (חוץ מהאינפוטים) אצל המנהל מערכת + מורה- ✔️
    דף מטלות אצל התלמיד- ✔️ (צריכה רק לאחר עם מה שנוי עשתה בקונטרולר היום).
    לסדר את הדיבגר ✔️✔️✔️✔️✔️
    דף הצגת מטלה אצל התלמיד -  🔸אני באמצע פליז אל תגעו שם 🔸
    
    אשלח לכן בינתיים צילומי מסך גם..
    מחר/יותר מאוחר אסיים את ההצגת מטלה.

---
+++ DocuProject/Controllers/StudentController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web;
+using System.Web.Hosting;
+using DocuProject.Models;
+using System.Web.Configuration;
+using System.Data;
+using System.Text;
+
+namespace DocuProject.Controllers
+{
+    public class StudentController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        [HttpPost] // ניהול תלמידים - קבלת טבלת תלמידים
+        [Route("api/Student/GetStudents")]
+        public DataTable GetS([FromBody]Student studentObj)
+        {
+            return studentObj.GetStudents();
+        }
+
+        [HttpGet]
+        [Route("api/Student/GetTasks/{name}/{num}/{prof}")]
+        public List<Task> GetTask(string name, string num, string prof)
+        {
+            Task T = new Task();
+            return T.ReadTask(name, num, prof); // Read from Models/Counrty
+        }
+
+        [HttpGet]
+        [Route("api/Student/specifictask/{class1}/{numClass}/{sub}/{topic}")]
+        public string getSpecificTask(string class1, string numClass, string sub, string topic)
+        {
+            Task task = new Task();
+            return task.getSpecificTask(class1, numClass, sub, topic);
+        }
+        [HttpGet]
+        [Route("api/Student/GetClassSubj/{name}/{num}")]
+        public List<ClassSubjects> GetCS(string name, string num)
+        {
+            ClassSubjects CS = new ClassSubjects();
+            return CS.ReadCS(name, num); // Read from Models/Counrty
+        }
+
+        [HttpGet]
+        [Route("api/Student/GetGetDataTask/{name}/{num}/{date}")]
+        public List<Group_Feedback> GetDT(string name, int num, DateTime date)
+        {
+            Group_Feedback DT = new Group_Feedback();
+            return DT.ReadDT(name, num, date); // Read from Models/Counrty
+        }
+    }
+
+}
